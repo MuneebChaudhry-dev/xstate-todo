083873c0f20f8d18fb7dc1ae8c7c28828ee2717e
Muneeb <muneebchaudhry.dev@gmail.com>
2023-11-24 15:42:57 +0500

    First Machine Program

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,35 @@
+'use client';
+import React from 'react';
+import { useMachine } from '@xstate/react';
+import { myMachine } from '../../machines/myFirstMachine';
+
 export default function Home() {
-  return <div>Hello World</div>;
+  const [state, send] = useMachine(myMachine);
+
+  return (
+    <div className='flex flex-col justify-center items-center w-full'>
+      <div className='w-full justify-center mb-4'>
+        <h1>{JSON.stringify(state.value)}</h1>
+      </div>
+
+      <div className='w-full flex justify-start'>
+        <button
+          className='bg-blue-500 text-white px-4 py-2 rounded'
+          onClick={() => {
+            send('MOUSEOVER');
+          }}
+        >
+          Over
+        </button>
+        <button
+          className='bg-red-500 text-white px-4 py-2 rounded'
+          onClick={() => {
+            send('MOUSEOUT');
+          }}
+        >
+          Out
+        </button>
+      </div>
+    </div>
+  );
 }
